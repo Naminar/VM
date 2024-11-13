837245070e0c46bcf608b747ac724c08e3510a29
Zhelyapov Aleksey <zhelyapov.aleksey@huawei-partners.com>
2024-11-13 14:07:07 +0300

    working

--- runtime_core/interpretator.cpp
+++ runtime_core/interpretator.cpp
@@ -1,19 +1,36 @@
+
 #include <string>
+#include <stack>
+#include <vector>
+#include <iostream>
+
+
+class Frame {
+  public:
+    Frame(const uint64_t n_regs):  _n_regs(n_regs) {
+        _regs.resize(n_regs);
+    }
+
+    std::vector<int64_t> _regs;
+    uint64_t _n_regs;
+};
 
 
 class Interpretator {
   public:
-    IP = 0;
-    RET_VAL = 0;
-    REG_FILE[128];
 
-    void *bytecode = nullptr;
+    std::stack<Frame> frames;
+    uint8_t *bytecode = nullptr;
+    uint8_t *ip = nullptr;
+    int64_t return_value = 0;
 
-    void Run(void *ptr, uint64_t len) {
-        bytecode = ptr;
-
-        while (IP < bytecode + len) {
-            
+    int64_t &Reg(const uint64_t n) {
+        Frame &frame = frames.top();
+        if (n > frame._n_regs - 1) {
+            throw std::runtime_error("Out of bounds in registers.");
         }
+        return frames.top()._regs[n];
     }
+
+    #include "isa_run.h"
 };
